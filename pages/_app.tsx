63375ada1979282a0c98558f7598f1cfbb2a60d0
Othman Shareef <othmanosx@gmail.com>
2022-08-09 15:33:24 +0300

    Feat: enable dark mode by default

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import { ChakraProvider } from "@chakra-ui/react"
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"
+import theme from "../theme"
 
 function MyApp({ Component, pageProps }: AppProps) {
   const client = new ApolloClient({
@@ -10,7 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   })
   return (
     <ApolloProvider client={client}>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <Component {...pageProps} />
       </ChakraProvider>
     </ApolloProvider>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import { Flex } from "@chakra-ui/react"
 import { NextPage } from "next"
 import Pools from "../components/Pools"
 import { Container } from "@chakra-ui/react"
